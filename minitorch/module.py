69d3ebabde421cef294ca91fad44dfadbe7f0051
Sophie Strassmann-Peña <sophie.strassmann@mail.mcgill.ca>
2025-05-19 19:19:14 -0400

    tada

--- minitorch/module.py
+++ minitorch/module.py
@@ -32,6 +32,7 @@ class Module:
     def train(self) -> None:
         """Set the mode of this module and all descendent modules to `train`."""
         # TODO: Implement for Task 0.4.
+       # tada
         raise NotImplementedError("Need to implement for Task 0.4")
 
     def eval(self) -> None:
